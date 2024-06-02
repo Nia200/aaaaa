f5701d68de940e983311631de6c5e89da1ebf82e
Nia200 <158120761+Nia200@users.noreply.github.com>
2024-06-02 22:42:24 +0600

    Add files via upload

--- firebase.config.js
+++ firebase.config.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCPS5JefWXy7Ij0gm-zcDEouBUyN66geT0",
   authDomain: "food-order-dfcd8.firebaseapp.com",
@@ -17,7 +13,6 @@ const firebaseConfig = {
   appId: "1:171553363799:web:f8bf6137ebd95753d11012",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
